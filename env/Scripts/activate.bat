f6d27aa900c5a5b14ef7b98ddb109006056b5009
Jenisis-03 <111039096+Jenisis-03@users.noreply.github.com>
2024-06-09 21:13:48 +0530

    login page updated

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Subham\Desktop\crud\env
+set VIRTUAL_ENV=C:\Users\Subham\Desktop\RMS\env
 
 if not defined PROMPT set PROMPT=$P$G
 
